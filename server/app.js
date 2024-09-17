fd4524bb755b7e8dddf7ebeac27b0bcaf7259fd7
ARPCharlie <charlie@arp-lan.com>
2024-09-17 21:57:50 +0200

    Chore: cleanup
    cleanup useless code

--- server/app.js
+++ server/app.js
@@ -5,8 +5,6 @@ const redis = require('redis');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
 // Create a Redis client
 const redisClient = redis.createClient({
   url: 'redis://localhost:6379'
@@ -29,15 +27,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routes
-const authRoutes = require('./routes/auth.js');
-app.use('/auth', authRoutes);
-
-const itemRoutes = require('./routes/items.js')
-app.use('/items', itemRoutes);
+// Routes
+const itemRoutes = require('./routes/items')(redisClient); // Pass the Redis client to the routes
+app.use('/api/items', itemRoutes);
 
 
-// start server
+// Start server
 app.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`);
 });
